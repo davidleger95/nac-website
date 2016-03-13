abc08f0235fbb5741cf7881c73a273cfce8fd584
David Leger <davidleger95@me.com>
2016-03-13 14:03:19 -0300

    Added smooth scrolling to anchor.

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,4 +18,17 @@
             "transform": trans
         });
     });
+    
+    $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - window.innerHeight/3
+        }, 1000);
+        return false;
+      }
+    }
+  });
 })();
