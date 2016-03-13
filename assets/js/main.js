b34ca39aa2507108ea5e27ecc4252113f66ced2b
David Leger <davidleger95@me.com>
2016-03-13 14:00:04 -0300

    JS refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /**************************************************
  *
- * Functions for [project name]
+ * Functions for New Age Creators
  * AUTHOR   David Leger
  *          www.davidleger.me
  *
@@ -9,10 +9,8 @@
 (function(){
     'use strict';
     var x, y, trans;
-    console.log("hey");
     
     $(window).on('mousemove', function(e){
-        console.log("hey");
         x = (window.innerWidth/2 - e.pageX)/30;
         y = (window.innerHeight/2 - e.pageY)/30;
         trans = "translate("+x+"px,"+y+"px)";
